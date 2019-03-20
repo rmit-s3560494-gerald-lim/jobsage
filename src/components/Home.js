6f903e43e8830579dd1e8172c2ae8bad942efec0
rmit-s3560494-gerald-lim <s3560494@student.rmit.edu.au>
2019-03-20 22:57:48 +1100

    draft home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import logo from '../logo.png';
 import './App.css';
 
 class Home extends Component {
     render() {
         return (
-            <header>Homepage</header>
+            <header className="App-header">PPMM - Employer & Job Seeker Matchmaking Platform
+            <img src={logo} className="logo" alt="logo" />
+            </header>
         )
     }
 }
+
+export default Home;
