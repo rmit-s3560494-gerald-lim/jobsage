e7ca69850803f0b1a11c75e59160fc5b22eab189
Matthew <matthew.ty.lu@gmail.com>
2019-03-25 14:22:48 +1100

    LoginPage (Matt)

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './HomePage';
+import LoginPage from './LoginPage';
+import SignUpPage from './SignUpPage';
 
 class App extends Component {
   render() {
     return (
       <Switch>
-        {/* <Route exact path='/' component={LoginPage} /> */}
+        <Route exact path='/' component={LoginPage} />
+        <Route path='/signup' component={SignUpPage} />
         <Route path='/home' component={HomePage} />
         {/* <Route path='/jobs' component={JobPage} /> */}
         {/* <Route path='/profile' component={ProfilePage} /> */}
---
+++ src/components/SignUpPage.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class SignUpPage extends Component {
+
+  render() {
+    return(
+
+        <h1>I am here</h1>
+        // <div class="form-group">
+        // <formgroup controlId="email">
+        // <FormLabel>Email</FormLabel>
+        // <FormControl
+        // type="email"
+        // value=""
+        // />
+        // </FormGroup>
+        // </form>
+        // <div>test</div>
+    );
+  }
+}
+
+export default SignUpPage;
