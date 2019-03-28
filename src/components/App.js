f4c58867675d03b0cb90e90bff6ae2eb930fed14
richardkuoch <s3563242@student.rmit.edu.au>
2019-03-28 18:50:50 +1100

    AdminLogin functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './HomePage';
+import AdminLogin from './AdminLogin';
 
 class App extends Component {
   render() {
@@ -8,7 +9,7 @@ class App extends Component {
       <Switch>
         {/* <Route exact path='/' component={LoginPage} /> */}
         <Route path='/home' component={HomePage} />
-        {/* <Route path='/jobs' component={JobPage} /> */}
+        <Route path='/adminlogin' component={AdminLogin} /> */}
         {/* <Route path='/profile' component={ProfilePage} /> */}
       </Switch>
     );
