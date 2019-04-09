6534accf8a7f1ecb29c781ff5e43cad0576b574d
rmit-s3560494-gerald-lim <s3560494@student.rmit.edu.au>
2019-04-10 01:06:06 +1000

    Filled in rest of display table

--- src/components/JobsList.js
+++ src/components/JobsList.js
@@ -8,6 +8,15 @@ const Jobs = props => (
         <td>{props.job.category}</td>
         <td>{props.job.city}</td>
         <td>{props.job.company_name}</td>
+        <td>{props.job.geo}</td>
+        <td>{props.job.job_board}</td>
+        {/* <td>{props.job.job_description}</td> */}
+        <td>{props.job.job_title}</td>
+        <td>{props.job.job_type}</td>
+        <td>{props.job.post_date}</td>
+        <td>{props.job.salary_offered}</td>
+        <td>{props.job.state}</td>
+        <td>{props.job.url}</td>
         <td>
             <Link to={"/edit/" + props.job._id}>Edit</Link>
         </td>
@@ -50,7 +59,7 @@ export default class JobsList extends Component {
                             <th>company_name</th>
                             <th>geo</th>
                             <th>job_board</th>
-                            <th>job_description</th>
+                            {/* <th>job_description</th> */}
                             <th>job_title</th>
                             <th>job_type</th>
                             <th>post_date</th>
