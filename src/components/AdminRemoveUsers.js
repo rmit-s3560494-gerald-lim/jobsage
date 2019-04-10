1351b29fba0bd4c2d8786bedaf10c76e8a6d81d5
richardkuoch <s3563242@student.rmit.edu.au>
2019-04-10 15:43:16 +1000

    "Added Admin can remove users code"

---
+++ src/components/AdminRemoveUsers.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+
+const Header = (props) => {
+    return(
+        <header>
+            <h1>{props.title}</h1>
+            <span className="stats">Users: {props.totalPlayers}</span>
+        </header>
+    );
+}
+      
+const Player = (props) => {
+    return(
+        <div className = "player">
+            <span className = "player-name">
+                <button className="remove-player" onClick={()=>props.removePlayer(props.id)}>x</button>
+                {props.name}
+            </span>
+
+            
+        </div>
+    );
+}
+
+class AdminRemoveUsers extends React.Component {
+    state = {
+        players: [
+            {
+                name: "Richard",
+                id: 1
+            },
+            {
+                name: "Alex",
+                id: 2
+            },
+            {
+                name: "Pearce",
+                id: 3
+            },
+            {
+                name: "James",
+                id: 4
+            }
+        ]
+    };
+
+    handleRemovePlayer = (id) => {
+        this.setState(prevState => {
+            return{
+                players: prevState.players.filter(p => p.id !== id) 
+            };
+        });
+    }
+
+    render(){
+        return(
+            <div className="userlist">
+                <Header 
+                    title="User Accounts" 
+                    totalPlayers={this.state.players.length}
+                />
+                
+                {this.state.players.map( player =>
+                    <Player
+                    name={player.name}
+                    id = {player.id}
+                    score={player.score}
+                    key={player.id.toString()}
+                    removePlayer = {this.handleRemovePlayer}
+                    />
+                )}
+                
+            </div>
+        );
+    }
+}
+
+export default AdminRemoveUsers;
+
+//ReactDOM.render(
+    //<App />,
+    //document.getElementById('root')
+//);
