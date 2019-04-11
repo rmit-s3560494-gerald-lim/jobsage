a9b0f2625c96d407b3470a8badf8328d7e3b095f
rmit-s3560494-gerald-lim <s3560494@student.rmit.edu.au>
2019-04-11 11:06:23 +1000

    Started writing tests for Database branch

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import JobsList from './JobsList';
 import CreateJobs from './CreateJobs';
@@ -7,13 +7,15 @@ import CreateJobs from './CreateJobs';
 class App extends Component {
   render() {
     return (
-      <Switch>
-        {/* <Route exact path='/' component={LoginPage} /> */}
-        <Route path='/home' component={HomePage} />
-        <Route path='/jobs' component={JobsList} />
-        <Route path='/add' component={CreateJobs} />
-        {/* <Route path='/profile' component={ProfilePage} /> */}
-      </Switch>
+      <BrowserRouter>
+        <Switch>
+          {/* <Route exact path='/' component={LoginPage} /> */}
+          <Route path='/home' component={HomePage} />
+          <Route path='/jobs' component={JobsList} />
+          <Route path='/add' component={CreateJobs} />
+          {/* <Route path='/profile' component={ProfilePage} /> */}
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
