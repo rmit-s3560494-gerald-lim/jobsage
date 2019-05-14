ad31549f6c090726b28628f5b4d88a677b7826d1
richardkuoch <s3563242@student.rmit.edu.au>
2019-05-14 17:56:46 +1000

    Job Seeker Profile

--- src/components/JobSeekerProfile.js
+++ src/components/JobSeekerProfile.js
@@ -16,17 +16,74 @@ class JobSeekerProfile extends Component{
                 <label htmlFor="Education">Education:</label> 
                 <input class="form-control" type="text" name="education" id="education"/>
               </div>
-              <div class="form-group">
-                <label htmlFor="Skills">Skills:</label> 
-                <input class="form-control" type="text" name="skills" id="skills"/>
-              </div>
-              <div class="form-group">
-                <label htmlFor="Proficiency">Proficiency:</label> 
-                <div class="slidecontainer">
-                    <input  class="form-control" type="range" min="0" max="2" value="1" id="myRange"/>
+
+              <div className="form-group">
+                <label>Skill 1: </label>
+                <div className = "form-group entry input-group col-xs-3">
+                <input className = "form-control" type="text" name = "skill2" id="skill2"/>
+                <select className = "form-control" name="Proficiency">
+                    <option disable selected value>--select an option--</option>
+                    <option value = "0">Beginner</option>
+                    <option value = "1">Intermediate</option>
+                    <option value = "2">Expert</option>
+                  </select>
                 </div>
               </div>
-              
+
+              <div className = "form-group">
+                <label>Skill 2: </label>
+                <div className = "form-group entry input-group col-xs-3">
+                <input className = "form-control" type="text" name = "skill3" id="skill3"/>
+                <select className = "form-control" name="Proficiency">
+                    <option disable selected value>--select an option--</option>
+                    <option value = "0">Beginner</option>
+                    <option value = "1">Intermediate</option>
+                    <option value = "2">Expert</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className = "form-group">
+                <label>Skill 3: </label>
+                <div className = "form-group entry input-group col-xs-3">
+                <input className = "form-control" type="text" name = "skill3" id="skill3"/>
+                <select className = "form-control" name="Proficiency">
+                    <option disable selected value>--select an option--</option>
+                    <option value = "0">Beginner</option>
+                    <option value = "1">Intermediate</option>
+                    <option value = "2">Expert</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className = "form-group">
+                <label>Skill 4: </label>
+                <div className = "form-group entry input-group col-xs-3">
+                <input className = "form-control" type="text" name = "skill3" id="skill3"/>
+                <select className = "form-control" name="Proficiency">
+                    <option disable selected value>--select an option--</option>
+                    <option value = "0">Beginner</option>
+                    <option value = "1">Intermediate</option>
+                    <option value = "2">Expert</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className = "form-group">
+                <label>Skill 5: </label>
+                <div className = "form-group entry input-group col-xs-3">
+                <input className = "form-control" type="text" name = "skill" id="skill"/>
+                <select className = "form-control" name="Proficiency">
+                    <option disable selected value>--select an option--</option>
+                    <option value = "0">Beginner</option>
+                    <option value = "1">Intermediate</option>
+                    <option value = "2">Expert</option>
+                  </select>
+                </div>
+              </div>
+              <div class="text-right">
+                <button className="btn btn-primary" type="button" float="right">Update</button>
+              </div>
             </form>
           </div>
         );
