470d0147225db47a9860809c451b136d6bf13790
KungaK <s3588773@student.rmit.edu.au>
2019-05-19 18:34:52 +1000

    Admin login page fix and style

--- src/components/AdminLogin.js
+++ src/components/AdminLogin.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import logo from '../logo.png';
 
-class AdminLogin extends Component{
-    
+const Error = props => <div id="userLoginError" class="alert alert-danger" role="alert">Username and Password invalid</div>
+class AdminLogin extends Component {
+
   constructor(props) {
     super(props);
 
-    this.state =  {
+    this.state = {
       username: '',
       password: '',
+      error: false,
     };
   }
 
@@ -25,15 +28,17 @@ class AdminLogin extends Component{
         userName = response.data.filter(obj => {
           return obj.user_name === this.state.username && obj.password === this.state.password;
         });
-        if(userName.length !== 0) {
+        if (userName.length !== 0) {
           localStorage.setItem('isAdminLoggedIn', 'true');
           this.props.history.push('/adminhomepage');
         }
       })
-      .catch(function(error) {
-        console.log(error.response);
+      .catch(error => {
+        this.setState({
+          error: true,
+        });
       })
-  } 
+  }
 
   handleSubmit = (e) => {
     e.preventDefault();
@@ -41,26 +46,29 @@ class AdminLogin extends Component{
   }
 
   render() {
-        return (
-          <div class="centerLogin">
-            <header className="App-header">
-            </header>
-            <form onSubmit={this.handleSubmit}>
-              <div className="form-group">
-              <label htmlFor="username">Username:</label> 
-              <input className="form-control" onChange={this.handleChange} type="text" name="username" id="username"/>
-              </div>
-              <div className="form-group">
-              <label htmlFor="password">Password:</label> 
-              <input className="form-control" onChange={this.handleChange} type="password" name="password" id="password"/>
-              </div>
-              <div className="text-right">
-              <button type="submit" class="btn btn-primary" value="Submit">Submit</button>
-              </div>
-            </form>
+    return (
+      <div class="centerLogin">
+        <img src={logo} class="img-fluid" alt="JobSage Logo" />
+        <h1 id="applicationTitle" class="display-3">JobSage</h1>
+        {this.state.error === true && (
+          <Error />
+        )}
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input className="form-control" onChange={this.handleChange} type="text" name="username" id="username" />
           </div>
-        );
-      }
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input className="form-control" onChange={this.handleChange} type="password" name="password" id="password" />
+          </div>
+          <div className="text-right">
+            <button type="submit" class="btn btn-primary" value="Submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default AdminLogin;
